36f1e22714b917d4f3932bbf039691578754b244
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2023-09-06 13:41:22 +0100

    project setup with tailwnd, and custom font

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>lotfi</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#322625',
+        grey: '#ebebeb',
+        blue: '#c0e3e5',
+        yellow: '#fdc936',
+      },
+    },
+  },
+  plugins: [],
+};
